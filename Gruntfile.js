6d79166839e99d1d757bf50d02a92b9b4d341a28
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-07 14:52:01 +0000

    Add test

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,26 @@ module.exports = function (grunt) {
             "main": "reg-exp-escaper",
             "global": "regExpEscaper"
         },
+        "watch": {
+            "js": {
+                "files": [
+                    "<%= config.main %>.js",
+                    "tags.js"
+                ],
+                "tasks": ["jshint"]
+            }
+        },
+        "jshint": {
+            "options": {
+                "jshintrc": ".jshintrc",
+                "reporter": require("jshint-stylish")
+            },
+            "all": [
+                "Gruntfile.js",
+                "<%= config.main %>.js",
+                "tags.js"
+            ]
+        },
         "babel": {
             "options": {
                 "sourceMap": true
@@ -67,6 +87,8 @@ module.exports = function (grunt) {
             }
         }
     });
+
+    grunt.task.registerTask("test", ["jshint:all"]);
 
     grunt.task.registerTask("build:es6", ["uglify:dist"]);
     grunt.task.registerTask("build:cjs", ["babel:dist", "uglify:distCjs"]);
